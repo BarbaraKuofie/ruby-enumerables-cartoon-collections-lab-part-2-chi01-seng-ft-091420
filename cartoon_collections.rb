eb507b36732041dc1dbb5b0819c67ff700b22dc0
Barbara Kuofie <barbara.kuofie@gmail.com>
2020-09-04 22:03:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,14 +8,14 @@ end
 def summon_captain_planet(planeteer_calls)
   # Use an Enumerable to capitalize and add '!' to every element in the passed in array
   # Return a new array of the results
-  planeteer_calls.map {|element|element.upcase + "!"}
+  planeteer_calls.map {|element|element.capitalize + "!"}
 end 
 
 
 def long_planeteer_calls(planeteer_calls)
   # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
   # Return the boolean result of this check
-  planeteer_calls.fin.any?{|word|word.length > 4}
+  planeteer_calls.any?{|word|word.length > 4}
 end 
 
 
@@ -24,5 +24,6 @@ def find_valid_calls(planeteer_calls)
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
   # Return the first valid call found, or return nil if no valid calls are found
   planeteer_calls.find {|word|word == valid_calls}
+end 
 end 
 
